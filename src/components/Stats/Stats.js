92bc326877f5fc7f49c32e0d697777db747fb19a
ThomasPalamara <toma06pal@gmail.com>
2019-01-28 20:17:08 +0000

    Correction fetches, craft added

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -15,66 +15,32 @@ class DisplayStats extends React.Component {
     componentDidUpdate(prevProps, prevState) {
 
         if ((this.props.realm.value && this.props.selectedRecipe._id) && (prevProps.realm.value !== this.props.realm.value || prevProps.selectedRecipe._id !== this.props.selectedRecipe._id)) {
-            let stats = new Object;
             let fetches = [];
-
             console.log('Fetching');
             this.setState({ loading: true }, () => {
                 console.log(this.state.loading, 'before fetch');
-                this.props.selectedRecipe.reagents.map(reagent => fetches.push(
-                    fetch(`${apiURL}/itemstats/${this.props.realm.value}/${reagent.blizzardId}`)
-                        .then(res => {
-                            if (!res.ok) {
-                                throw new Error("Not found");
-                            }
-                            return res.json()
-                        })
-                        .then(response => {
-                            stats[reagent.blizzardId] = response;
-                            console.log(stats);
-                            // this.setState(state => { return ({ stats: Object.assign({}, state.stats, stat) }) });
-                        })
-                ));
-                Promise.all(fetches).then(() => {
-                    this.setState({ loading: false, stats });
+                this.props.selectedRecipe.reagents.map(reagent => fetches.push(this.fetchItemStats(reagent.blizzardId)));
+                fetches.push(this.fetchItemStats(this.props.selectedRecipe.craft.blizzardId));
+                Promise.all(fetches).then((response) => {
+                    console.log(response);
+                    this.setState({ loading: false });
                     console.log('End Fetching');
                 });
             }
             );
-
-
-
         }
     }
 
-    async fetchAll() {
-        await this.fetchItemStats(this.props.selectedRecipe.craft.blizzardId);
-        console.log('ok2');
-        await this.props.selectedRecipe.reagents.map(reagent => this.fetchItemStats(reagent.blizzardId));
-    }
-
     fetchItemStats(itemId) {
-        fetch(`${apiURL}/itemstats/${this.props.realm.value}/${itemId}`)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error("Not found");
-                }
-                return res.json()
-            })
-            .then(response => {
-                let stat = {};
-                stat[itemId] = response;
-                console.log(this.state, 'test');
-                this.setState(state => { return ({ stats: Object.assign({}, state.stats, stat) }) });
-                // this.setState(prevState => ({
-                //     stats: {
-                //         ...prevState.stats,
-                //         stat
-                //     }
-                // }))
-                console.log('ok1');
-            });
+        return fetch(`${apiURL}/itemstats/${this.props.realm.value}/${itemId}`)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Not found");
+            }
+            return res.json()
+        })
     }
+    
     handleSelectedStats = (selectedStats) => {
         this.setState({ selectedStats })
     }
